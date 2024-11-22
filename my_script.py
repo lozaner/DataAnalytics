95af11cc3921f43e28f7d7449a0d824faa5e45d3
elpidio lozano <elpidio.ramirez1@gmail.com>
2024-11-22 13:59:39 -0600

    cargar carpeta notebook y eliminar duplicados

---
+++ my_script.py
@@ -0,0 +1,14 @@
+import os
+print(f'estoy trabajando en el directorio {os.getcwd()}')
+
+x=10
+
+if x%2==0:
+                print(f'{x} is an ever number')
+
+else:
+        print(f'{x} is an odd number')  
+
+
+
+
