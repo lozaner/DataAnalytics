f0a5f8ce2962cbab79931dcf0f4f8baa2f26ca6b
elpidio lozano <elpidio.ramirez1@gmail.com>
2024-11-25 21:00:33 -0600

    agregamos proyecto 7

--- portfolio-DA/proyecto-7/app.py
+++ portfolio-DA/proyecto-7/app.py
@@ -3,12 +3,17 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+
+st.header('Proyecto 7 - Herramientas de Desarrollo de Software')
+
+
+hist_button = 
+('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
 
 # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches puede ver la base de datos en mi Git Hub https://github.com/lozaner/notebooks/tree/main')
             
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
